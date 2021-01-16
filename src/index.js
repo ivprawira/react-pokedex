272dae0bbb8ca77e8ab85174c7b98a1d640eadb9
Ivan Prawira Limanauw <ivan.limanauw@gmail.com>
2021-01-16 14:00:02 +0700

    npm install apollo

--- src/index.js
+++ src/index.js
@@ -3,12 +3,29 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './pages/App';
 import reportWebVitals from './reportWebVitals';
+import { Route, BrowserRouter as Router, Switch } from "react-router-dom";
+import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
+
+const routing = (
+  <Router>
+    <Switch>
+      <Route exact path="/" component={App} />
+      {/* <Route path="/pokemonDetail" component={FirstPage} /> */}
+      {/* <Route component={NotFound} /> */}
+    </Switch>
+  </Router>
+);
+
+const client = new ApolloClient({
+  uri: "https://graphql-pokeapi.vercel.app/api/graphql",
+  cache: new InMemoryCache(),
+});
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  <ApolloProvider client={client}>
+    <React.StrictMode>{routing}</React.StrictMode>
+  </ApolloProvider>,
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
