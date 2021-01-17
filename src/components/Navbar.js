a2383663386ae48b06dcbdf44a2fdc83f9e20b15
Ivan Prawira Limanauw <ivan.limanauw@gmail.com>
2021-01-17 18:56:34 +0700

    updated navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,24 +1,31 @@
-import React from 'react'
-import '../assets/Navbar.css'
-import { css } from '@emotion/react'
+import React from "react";
+import { Link } from "react-router-dom";
 
-const navbarStyle = css({
-    display: 'inline-block',
-    color: 'red'
-})
-
+import "../assets/Navbar.css";
+import pokeball_icon from "../assets/icons/pokeball_icon.png";
+import pokedex_icon from "../assets/icons/pokedex_icon.png";
 
 const Navbar = (props) => {
-    return (
-        <div>
-            <div
-                css={navbarStyle}
-            >
-                <h1>Pokedex</h1>
-                <h1>{props.pageTitle}</h1>
-            </div>
+  return (
+    <div className="navbar">
+      <div className="navbar__title">
+        <div className="navbar__item">Pokedex</div>
+        <div className="navbar__item">{props.pageTitle}</div>
+      </div>
+      <div className="navbar__navigation">
+        <div className="navbar__item">
+          <Link to={`/`}>
+            <img src={pokedex_icon} alt="Pokedex" />
+            <p>Pokedex</p>
+          </Link>
         </div>
-    )
-}
+        <div className="navbar__item">
+          <img src={pokeball_icon} alt="Pokeball" />
+          <p>My Pokemon</p>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default Navbar;
