2eb059c3e16e7ec5345606d535c7fa1c6790756f
Ivan Prawira Limanauw <ivan.limanauw@gmail.com>
2021-01-17 22:27:18 +0700

    LoadingBar component with css

---
+++ src/components/LoadingBar.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+import "../assets/LoadingBar.css";
+
+const LoadingBar = () => {
+  return <div className="loading">Loading ...</div>;
+};
+
+export default LoadingBar;
