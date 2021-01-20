41c5216da11487193dcaa0a9f79085d05ea5dce2
Ivan Prawira Limanauw <ivan.limanauw@gmail.com>
2021-01-20 16:13:20 +0700

    idb-keyval with mypokemonlist useeffect

--- src/pages/MyPokemonList.js
+++ src/pages/MyPokemonList.js
@@ -1,32 +1,43 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import { entries } from 'idb-keyval'
 import "../assets/MyPokemonList.css"
 import Navbar from "../components/Navbar"
 import LoadingBar from "../components/LoadingBar"
 import PokemonTile from "../components/PokemonTile"
 
-const MyPokemonList = () => {
-  const [pokemonList, setPokemonList] = useState([])
 
-  entries().then( (entries) => 
-    setPokemonList(entries)
-  )
+const MyPokemonList = () => {
+  const [pokemonList, setPokemonList] = useState({ data: [] })
+
+  useEffect( () => {
+    const fetchData = async () => {
+      const result = await entries()
+      setPokemonList(result)
+    }
+    fetchData()
+  }, [])
+
+  console.log('AA')
+  console.log(pokemonList)
 
   return (
     <>
       <Navbar pageTitle="Pokédex" />
-      <div className="my-pokemon-list">
-        {pokemonList.map((pokemon, key) => {
-          return (
-            <PokemonTile
-              key={key}
-              name={pokemon[0]}
-              imageURL={pokemon[1].pokemon.sprites.front_default}
-            />
-          );
-        })}
-      </div>
-      {(pokemonList.length === 0) && <LoadingBar />}
+      {(pokemonList.length > 0) && (
+        <div className="my-pokemon-list">
+          {pokemonList.map((pokemon, key) => {
+            return (
+              <PokemonTile
+                key={key}
+                name={pokemon[0]}
+                imageURL={pokemon[1].pokemon.sprites.front_default}
+                isMyPokemon={true}
+              />
+            );
+          })}
+        </div>
+      )}
+      {(pokemonList.length < 1) && <LoadingBar />}
       
     </>   
   )
