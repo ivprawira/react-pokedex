ee510742042c43aa870b4fc53098fe4b2a3d6c85
Ivan Prawira Limanauw <ivan.limanauw@gmail.com>
2021-01-20 22:18:24 +0700

    added pokemon caught count

--- src/components/PokemonTile.js
+++ src/components/PokemonTile.js
@@ -3,12 +3,26 @@ import { Link } from "react-router-dom";
 
 import "../assets/PokemonTile.css";
 
-const PokemonTile = ({ name, imageURL, isMyPokemon = false }) => {
+const PokemonTile = ({ id, name, imageURL, myIdList = [], isMyPokemon = false }) => {
   const redirectLink = isMyPokemon ? `/myPokemonDetail/${name}` : `/pokemonDetail/${name}`
-
+  let count = 0
+  
+  if (!isMyPokemon) {
+    if (myIdList.includes(id)) {
+      myIdList.forEach( (myId) => {
+        if (myId === id) {count += 1}
+      })
+    }
+  }
+  
   return (
     <Link to={redirectLink}>
       <div className="pokemon-tile">
+        {!isMyPokemon && (
+          <div className="pokemon-tile__owned">
+            caught: {count}
+          </div>
+        )}
         <img src={imageURL} alt={`${name} Pokemon Profile`} />
         <p>{name}</p>
       </div>
--- src/pages/App.js
+++ src/pages/App.js
@@ -1,18 +1,32 @@
-import React, { useState } from "react";
-import "../assets/App.css";
-import Navbar from "../components/Navbar";
-import LoadingBar from "../components/LoadingBar";
+import React, { useState, useEffect } from "react";
 import { GET_POKEMON_LIST } from "../API/api.js";
 import { useQuery } from "@apollo/client";
+import { get } from 'idb-keyval'
+import Navbar from "../components/Navbar";
+import LoadingBar from "../components/LoadingBar";
 import PokemonTile from "../components/PokemonTile";
+import "../assets/App.css";
 
 const App = (props) => {
+  // pokemonList to contain API result
+  // variables for API call
   const [pokemonList, setPokemonList] = useState([]);
   const [variables, setVariables] = useState({
     limit: 30,
     offset: 0,
   });
 
+  // prepare my pokemon list to count owned pokemon numbers
+  const [myPokemonIdList, setMyPokemonIdList] = useState([])
+  useEffect( () => {
+    const fetchData = async () => {
+      const result = await get('')
+      setMyPokemonIdList(result)
+    }
+    fetchData()
+  }, [])
+
+  // API Call
   const { loading, error, refetch } = useQuery(GET_POKEMON_LIST, {
     variables,
     onCompleted: (res) => {
@@ -24,6 +38,7 @@ const App = (props) => {
     },
   });
 
+  // For Refetch
   const getNextList = () => {
     setVariables({
       ...variables,
@@ -43,8 +58,10 @@ const App = (props) => {
               return (
                 <PokemonTile
                   key={key}
+                  id={key+1}
                   name={pokemon.name}
                   imageURL={pokemon.image}
+                  myIdList={myPokemonIdList}
                 />
               );
             })}
